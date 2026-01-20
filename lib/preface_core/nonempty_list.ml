d797979da2aa8c3890f619d4b796164363094b4e
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-01-20 17:00:27 +0100

    Manual code review of [lib/preface_core/nonempty_list.ml]
    I started from the code generated by Claude and made some changes based on how I
    would have preferred to write it.
    
    I didn't review the other files yet.

--- lib/preface_core/nonempty_list.ml
+++ lib/preface_core/nonempty_list.ml
@@ -3,55 +3,55 @@ type 'a t = ( :: ) of 'a * 'a list
 let create x = x :: []
 
 let rev_append (x :: xs) (y :: ys) =
-  match List.rev_append List.(x :: xs) List.(y :: ys) with
+  match List.rev_append xs (x :: y :: ys) with
   | [] -> assert false
-  | List.(z :: zs) -> z :: zs
+  | z :: zs -> z :: zs
 ;;
 
 let hd (x :: _) = x
-let tl = function _ :: [] -> None | _ :: y :: ys -> Some (y :: ys)
+let tl (_ :: xs) = match xs with [] -> None | y :: ys -> Some (y :: ys)
 
 let rev (x :: xs) =
-  match List.(rev (x :: xs)) with
-  | [] -> assert false
-  | List.(y :: ys) -> y :: ys
+  match List.rev (x :: xs) with [] -> assert false | y :: ys -> y :: ys
 ;;
 
-let from_list = function [] -> None | List.(x :: xs) -> Some (x :: xs)
+let from_list = function [] -> None | x :: xs -> Some (x :: xs)
 let to_list (x :: xs) = List.(x :: xs)
 let length (_ :: xs) = 1 + List.length xs
-let cons x (y :: ys) = x :: List.(y :: ys)
-let iteri f (x :: xs) = List.iteri f List.(x :: xs)
-let iter f (x :: xs) = List.iter f List.(x :: xs)
+let cons x (y :: ys) = x :: y :: ys
+let iteri f (x :: xs) = List.iteri f (x :: xs)
+
+let iter f (x :: xs) =
+  f x;
+  List.iter f xs
+;;
 
 let mapi f (x :: xs) =
-  match List.mapi f List.(x :: xs) with
-  | [] -> assert false
-  | List.(y :: ys) -> y :: ys
+  match List.mapi f (x :: xs) with [] -> assert false | y :: ys -> y :: ys
 ;;
 
 let map f (x :: xs) =
-  match List.map f List.(x :: xs) with
-  | [] -> assert false
-  | List.(y :: ys) -> y :: ys
+  let y = f x in
+  y :: List.map f xs
 ;;
 
-let fold_left f acc (x :: xs) = List.fold_left f acc List.(x :: xs)
+let fold_left f acc (x :: xs) = List.fold_left f (f acc x) xs
 let reduce f (x :: xs) = List.fold_left f x xs
-let fold_right f (x :: xs) acc = List.fold_right f List.(x :: xs) acc
-let append (x :: xs) (y :: ys) = x :: List.append xs List.(y :: ys)
-let flatten nel = reduce append nel
+let fold_right f (x :: xs) acc = List.fold_right f (x :: xs) acc
+let append (x :: xs) (y :: ys) = x :: List.append xs (y :: ys)
 
-let equal f (x :: xs) (y :: ys) =
-  f x y
-  &&
-  let rec aux = function
-    | [], [] -> true
-    | List.(a :: as'), List.(b :: bs') -> f a b && aux (as', bs')
-    | _ -> false
-  in
-  aux (xs, ys)
+let[@tail_mod_cons] rec flatten_list_of_nels = function
+  | [] -> []
+  | (x :: xs) :: ys -> x :: append_to_nels xs ys
+
+and[@tail_mod_cons] append_to_nels xs ys =
+  match xs with
+  | [] -> flatten_list_of_nels ys
+  | x :: xs -> x :: append_to_nels xs ys
 ;;
+
+let flatten ((x :: xs) :: ys) = x :: append_to_nels xs ys
+let equal f (x :: xs) (y :: ys) = f x y && List.equal f xs ys
 
 let pp pp' formater list =
   let pp_sep ppf () = Format.fprintf ppf ";@ " in
