64d3d7fd707e93220cd6c21d58c67eb1567e395f
Mathieu Barbin <mathieu.barbin@gmail.com>
2026-01-29 15:18:16 +0100

    Avoid nonreachable assert false in rev and rev_append
    From a code review suggestion by gr-im

--- lib/preface_core/nonempty_list.ml
+++ lib/preface_core/nonempty_list.ml
@@ -1,22 +1,21 @@
 type 'a t = ( :: ) of 'a * 'a list
 
 let create x = x :: []
+let to_list (x :: xs) = List.(x :: xs)
 
-let rev_append (x :: xs) (y :: ys) =
-  match List.rev_append xs (x :: y :: ys) with
-  | [] -> assert false
-  | z :: zs -> z :: zs
+let rev_append_to_list (x :: xs) l2 =
+  let rec aux acc first = function
+    | List.[] -> first :: acc
+    | List.(x :: xs) -> aux (first :: acc) x xs
+  in
+  aux l2 x xs
 ;;
 
+let rev_append t1 t2 = rev_append_to_list t1 (to_list t2)
 let hd (x :: _) = x
 let tl (_ :: xs) = match xs with [] -> None | y :: ys -> Some (y :: ys)
-
-let rev (x :: xs) =
-  match List.rev (x :: xs) with [] -> assert false | y :: ys -> y :: ys
-;;
-
+let rev t = rev_append_to_list t []
 let from_list = function [] -> None | x :: xs -> Some (x :: xs)
-let to_list (x :: xs) = List.(x :: xs)
 let length (_ :: xs) = 1 + List.length xs
 let cons x (y :: ys) = x :: y :: ys
 let iteri f (x :: xs) = List.iteri f (x :: xs)
